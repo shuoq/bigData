2c46c65120f7d9d7c8936056b88b820cbc8d744e
shuoq <shuoq@utas.edu.au>
2020-05-15 14:19:09 +0800

    新建分支

--- Redis/src/main/java/git.java
+++ Redis/src/main/java/git.java
@@ -2,5 +2,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("helloGit");
         System.out.println("更新1");
+        System.out.println("新建分支");
     }
 }
